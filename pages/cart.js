4aaca8c8b51886bdbbc424ca644ce46acefa1f29
Dharmesh Bokadiya <dharmeshbo16@gmail.com>
2023-04-21 15:24:24 +0530

    change

--- pages/cart.js
+++ pages/cart.js
@@ -1,18 +1,12 @@
 import Cart from '@/components/Cart'
 import LayoutWrapper from '@/shared/LayoutWrapper'
 import CartFooter from '@/shared/CartFooter'
-import MainHeader from '@/shared/MainHeader'
-import useAppContext from '@/hooks/useAppContext';
-import AppContext from '@/utils/appContext';
 
 export default function CartIndex() {
-  const value = useAppContext();
   return (
-    <AppContext.Provider value={value}>
     <LayoutWrapper>
       <Cart />
       <CartFooter />
     </LayoutWrapper>
-    </AppContext.Provider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Home from '@/components/Home'
 import LayoutWrapper from '@/shared/LayoutWrapper'
 import MainFooter from '@/shared/MainFooter'
+import useAppContext from '@/hooks/useAppContext';
+import AppContext from '@/utils/appContext';
 
 export default function HomeIndex() {
+  const value = useAppContext();
   return (
+    <AppContext.Provider value={value}>
     <LayoutWrapper>
       <Home />
       <MainFooter />
     </LayoutWrapper>
+    </AppContext.Provider>
   )
 }
