4cbcb3e044d2482fa7ccf9f162d0d4444b9bd0a4
Dharmesh Bokadiya <dharmeshbo16@gmail.com>
2023-04-21 15:27:08 +0530

    changes

--- shared/CartFooter.jsx
+++ shared/CartFooter.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { TbArrowRight } from "react-icons/tb";
 import { useRouter } from "next/router";
 import { CART_ITEMS } from "@/utils/constant";
+import routes from "@/utils/routes";
 
 const CartFooter = () => {
   const router = useRouter();
@@ -12,6 +13,10 @@ const CartFooter = () => {
     setCartCount(cartLength || 0)
   }, [])
 
+  const placeOrder = () => {
+    localStorage.clear()
+    router.push(routes.home)
+  }
   return (
     <div className="absolute bottom-0 left-0 w-full p-2  rounded-t-lg">
       <div className="flex items-center justify-between rounded-lg p-2 gap-2 bg-gradient-to-r from-[#459EAF] to-[#007991] shadow-cartfoot ">
@@ -19,7 +24,7 @@ const CartFooter = () => {
         <div className="relative flex items-center gap-3">
           <p className="uppercase text-white font-medium text-[13px] leading-normal">PLACE ORDER</p>
           <button className="h-7 w-7 rounded-full bg-gradient-to-l from-[#459EAF] to-[#007991] shadow-cartbtn flex items-center justify-center text-white text-lg">
-            <TbArrowRight />
+            <TbArrowRight onClick={() => placeOrder()} />
           </button>
         </div>
       </div>
